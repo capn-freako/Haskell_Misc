089ce499f00b3c49257b8c7c6fc612388fa4c2a3
David Banas <capn.freako@gmail.com>
2015-07-03 19:06:54 -0700

    Minor clean-up.

--- norm_by_eval.hs
+++ norm_by_eval.hs
@@ -3,8 +3,9 @@
 
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE RankNTypes #-}
+{-# OPTIONS_GHC -Wall #-}
 
-module NormForm where
+module Main where
 
 newtype Base = In{out :: Term Base}
 
@@ -26,12 +27,20 @@ reify RBase v = out v
 reify (ra :-> rb) v = Fun (reify rb . v . reflect ra)
 
 reflect :: forall t. Type t -> Term t -> t
-reflect RBase e = In e
-reflect (ra :-> rb) e = reflect rb . App e . reify ra
+reflect RBase expr = In expr
+reflect (ra :-> rb) expr = reflect rb . App expr . reify ra
 
 -- Exercise 12 - Implement show() for 'Term t'.
 instance Show (Term t) where
-    show (Var s) = s
-    show (App f x) = "App (" ++ show f ++ "(" ++ show x ++ "))"
+    show (Var str) = str
+    show (App f x) = "App " ++ show f ++ " (" ++ show x ++ ")"
     show (Fun rf) = "Fun (\\a -> " ++ show (rf (Var "a")) ++ ")"
+
+s x y z = x z (y z)
+k x y = x
+i x = x
+e =s (s (k s) (s (k k) i))((s ((s (k s))((s (k k))i)))(k i))
+main = do print $ reify (b :-> b) (s k k)
+          print $ reify (b :-> (b :-> b)) (s (k k)i)
+          print $ reify ((b :-> b) :-> (b :-> b)) e
 
