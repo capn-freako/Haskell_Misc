38c627d0b157bf19ca30eda69466c73e429bf23c
David Banas <capn.freako@gmail.com>
2015-09-19 08:31:24 -0700

    Completed exercise 3.2.1.

--- Typeclassopedia/ex_3.2.1.hs
+++ Typeclassopedia/ex_3.2.1.hs
@@ -21,9 +21,18 @@ instance MyFunctor (Either e) where
     fmap' g (Left e)  = Left e
     fmap' g (Right x) = Right (g x)
 
+-- Functor ((->) e)
+instance MyFunctor ((->) e) where
+    fmap' g f = g . f
+
 -- Testing Functor law on defined instances.
 prop_either_test testVal = fmap' id testVal == testVal  -- "id testVal" was producing an annoying error.
     where types = testVal :: (Either String Int)
+
+prop_function_test testVal = fmap' id testFunc testVal == testFunc testVal
+    where testFunc :: Int -> Int
+          testFunc x = 2 * x
+          types = testVal :: Int
 
 return []  -- See comments above 'quickCheckAll', in http://hackage.haskell.org/package/QuickCheck-2.8/docs/src/Test-QuickCheck-All.html
 runTests = $quickCheckAll 
