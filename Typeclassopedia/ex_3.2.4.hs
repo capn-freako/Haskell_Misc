6d2548e2a039df32668338d18ba64693a8cb78a7
David Banas <capn.freako@gmail.com>
2015-09-21 15:46:00 -0700

    After completing Ex. 3.2.4

--- Typeclassopedia/ex_3.2.4.hs
+++ Typeclassopedia/ex_3.2.4.hs
@@ -1,7 +1,7 @@
--- Typeclassopedia - Sec. 3.2, Exercise 3.
+-- Typeclassopedia - Sec. 3.2, Exercise 4.
 --
 -- Original author: David Banas <capn.freako@gmail.com>
--- Original date:   September 20, 2015
+-- Original date:   September 21, 2015
 --
 -- Copyright (c) 2015 David Banas; all rights reserved World wide.
 
@@ -18,36 +18,6 @@ import Test.QuickCheck.All (quickCheckAll, verboseCheckAll)
 class MyFunctor f where
     fmap' :: (a -> b) -> f a -> f b
 
-data ITree a = Leaf (Int -> a) 
-             | Node [ITree a]
-
-instance Eq a => Eq (ITree a) where
-    Leaf g == Leaf h = g 0 == h 0
-    Node xs == Node ys = and (zipWith (==) xs ys)
-    Leaf _ == Node _ = False
-    Node _ == Leaf _ = False
-
-instance Show (ITree a) where
-    show _ = "No representation, yet."
-
--- Functor ITree
-instance MyFunctor ITree where
-    fmap' g (Leaf f)   = Leaf (g . f)
-    fmap' g (Node its) = Node $ map (fmap' g) its
-
-instance (Num a, Random a) => Arbitrary (ITree a) where
-    arbitrary = return $ Node [Leaf fromIntegral, Node [Leaf fromIntegral, Leaf fromIntegral]]
-
--- Testing Functor law on defined instances.
-prop_itree_test testVal = fmap' id testVal == testVal  -- "id testVal" was producing an annoying error.
-    where types = testVal :: ITree Int
-
-return []  -- See comments above 'quickCheckAll', in http://hackage.haskell.org/package/QuickCheck-2.8/docs/src/Test-QuickCheck-All.html
-runTests = $quickCheckAll 
--- runTests = $verboseCheckAll 
-
-main :: IO ()
-main = do
-    allPass <- runTests
-    unless allPass exitFailure
+-- Can't be made a functor, due to 'Int' type restriction.
+data NoFunc Int = NoFunc Int
 
