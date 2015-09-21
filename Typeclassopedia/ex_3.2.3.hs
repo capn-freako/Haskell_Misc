575b731ea031ca596ea1ede96c7e05054cbbba08
David Banas <capn.freako@gmail.com>
2015-09-21 09:01:16 -0700

    Completed exercise 3.2.3.

--- Typeclassopedia/ex_3.2.3.hs
+++ Typeclassopedia/ex_3.2.3.hs
@@ -1,58 +1,46 @@
--- Typeclassopedia - Sec. 3.2, Exercise 2.
+-- Typeclassopedia - Sec. 3.2, Exercise 3.
 --
 -- Original author: David Banas <capn.freako@gmail.com>
 -- Original date:   September 20, 2015
 --
 -- Copyright (c) 2015 David Banas; all rights reserved World wide.
---
--- Discussion, as per exercise instructions:
---  Similarities: Both types/instances return a pair of values.
---  Differences:
---    - The type of the first member of the pair can differ from that
---      of the second, in the '(,) e' case.
---    - The mapped function, g, is only mapped over the second member
---      of the pair, in the '(,) e' case.
---    - The type of the second member of the pair is NOT included in
---      the Functor instance, for either type, but the type of the
---      first member IS included, for ((,) e), but NOT for Pair.
 
 {-# LANGUAGE TemplateHaskell #-}
 
 module Main where
 
--- import Test.QuickCheck
 import Control.Monad (unless)
 import System.Exit (exitFailure)
 import System.Random (Random)
-import Test.QuickCheck (choose, vectorOf, elements, collect)
 import Test.QuickCheck.Arbitrary
 import Test.QuickCheck.All (quickCheckAll, verboseCheckAll)
 
 class MyFunctor f where
     fmap' :: (a -> b) -> f a -> f b
 
--- Functor ((,) e)
-instance MyFunctor ((,) e) where
-    fmap' g (e, a) = (e, g a)
+data ITree a = Leaf (Int -> a) 
+             | Node [ITree a]
 
-data Pair a = Pair a a deriving (Eq, Show)
+instance Eq a => Eq (ITree a) where
+    Leaf g == Leaf h = g 0 == h 0
+    Node xs == Node ys = and (zipWith (==) xs ys)
+    Leaf _ == Node _ = False
+    Node _ == Leaf _ = False
 
-instance (Num a, Random a) => Arbitrary (Pair a) where
-    arbitrary = do
-        x <- choose (0, 99)
-        y <- choose (0, 99)
-        return (Pair x y)
+instance Show (ITree a) where
+    show _ = "No representation, yet."
 
--- Functor (Pair a)
-instance MyFunctor Pair where
-    fmap' g (Pair x y) = Pair (g x) (g y)
+-- Functor ITree
+instance MyFunctor ITree where
+    fmap' g (Leaf f)   = Leaf (g . f)
+    fmap' g (Node its) = Node $ map (fmap' g) its
+
+instance (Num a, Random a) => Arbitrary (ITree a) where
+    arbitrary = return $ Node [Leaf fromIntegral, Node [Leaf fromIntegral, Leaf fromIntegral]]
 
 -- Testing Functor law on defined instances.
-prop_comma_test testVal = fmap' id testVal == testVal  -- "id testVal" was producing an annoying error.
-    where types = testVal :: (String, Int)
-
-prop_pair_test testVal = fmap' id testVal == testVal
-    where types = testVal :: Pair Int
+prop_itree_test testVal = fmap' id testVal == testVal  -- "id testVal" was producing an annoying error.
+    where types = testVal :: ITree Int
 
 return []  -- See comments above 'quickCheckAll', in http://hackage.haskell.org/package/QuickCheck-2.8/docs/src/Test-QuickCheck-All.html
 runTests = $quickCheckAll 
