d4dd9e5fb9442309a9c5bb59d5c52b9fa3993ace
David Banas <capn.freako@gmail.com>
2016-05-31 21:05:59 -0700

    Completed Simple Algebraic Data Types.

--- shape.cpp
+++ shape.cpp
@@ -7,6 +7,7 @@ using namespace std;
 class Shape {
   public:
     virtual double area() const = 0;
+    virtual double circ() const = 0;
 };
 
 class Rectangle : public Shape {
@@ -22,6 +23,7 @@ class Rectangle : public Shape {
     }
 
     double area() const { return (_height * _width); }
+    double circ() const { return (2.0 * (_height + _width)); }
     double height() const { return _height; }
     double width() const { return _width; }
 
@@ -40,6 +42,7 @@ class Circle : public Shape {
     }
 
     double area() const { return (PI * _radius * _radius); }
+    double circ() const { return (2.0 * PI * _radius); }
     double radius() const { return _radius; }
 
   private:
@@ -49,11 +52,15 @@ class Circle : public Shape {
 int main(int argc, char *argv[]) {
     auto r1 = Rectangle(2.0, 4.0);
     cout << "Area of r1: " << r1.area() << endl;
+    cout << "Circumfrence of r1: " << r1.circ() << endl;
     auto r2 = Rectangle(r1);
     cout << "Area of r2: " << r2.area() << endl;
+    cout << "Circumfrence of r2: " << r2.circ() << endl;
     auto c1 = Circle(2.0);
     cout << "Area of c1: " << c1.area() << endl;
+    cout << "Circumfrence of c1: " << c1.circ() << endl;
     auto c2 = Circle(c1);
     cout << "Area of c2: " << c2.area() << endl;
+    cout << "Circumfrence of c2: " << c2.circ() << endl;
 }
 
